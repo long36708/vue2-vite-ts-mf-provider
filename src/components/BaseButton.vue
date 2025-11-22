8b107d261e0d75ff0cae28293e7330a1810e26dd
longmo <3131521901@qq.com>
2025-11-22 19:49:38 +0800

    test git husky

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,18 +1,12 @@
 <!--
  * @Author: longmo
  * @Date: 2025-11-17 23:33:56
- * @LastEditTime: 2025-11-17 23:34:09
+ * @LastEditTime: 2025-11-22 19:41:18
  * @FilePath: src/components/BaseButton.vue
  * @Description:
  -->
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-<button>BaseButton</button>
+  <button>BaseButton</button>
 </template>
-
-<style scoped>
-
-</style>
